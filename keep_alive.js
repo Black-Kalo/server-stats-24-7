6d7b0b2eb5a77cfaf52e4a98ea4a0a1aa99c1e93
天然呆幻月 <64715639+Tira-tw@users.noreply.github.com>
2022-07-02 17:33:13 +0800

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.write("now bot 24/7! , use Uptime Robot can 24/7! Uptime Robot > https://uptimerobot.com/ <  Join Support >> https://discord.gg/n5x4gr32 <<");
-  res.end();
-}).listen(8080);
